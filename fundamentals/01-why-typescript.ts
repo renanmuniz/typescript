fe2a64c93230139ecd3a266b17bc336aa8a3bd3e
Renan Muniz <renan.mmerlo@gmail.com>
2025-12-11 01:41:54 -0300

    Initiated TypeScript course. Done Section 1.

---
+++ fundamentals/01-why-typescript.ts
@@ -0,0 +1,9 @@
+const courseName = "Typescript Bootcamp V3";
+printCourseName(courseName);
+
+function printCourseName(name :string) {
+    console.log("The course name is  " + name.toUpperCase());
+}
+
+
+
