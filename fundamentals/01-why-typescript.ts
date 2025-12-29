b70e167f0f9625bf865ca421db0a128e905c6063
Renan Muniz <renan.mmerlo@gmail.com>
2025-12-29 10:31:33 -0300

    Finished section 1

--- fundamentals/01-why-typescript.ts
+++ fundamentals/01-why-typescript.ts
@@ -1,9 +1,11 @@
 const courseName = "Typescript Bootcamp V3";
+//courseName = "abc123"; // This will cause an error because courseName is a constant
+
+
+
 printCourseName(courseName);
 
 function printCourseName(name :string) {
     console.log("The course name is  " + name.toUpperCase());
 }
-
-
 
