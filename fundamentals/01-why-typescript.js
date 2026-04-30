657e222b5d8052fc16249cefccfc27e3c1fc3bba
Renan Merlo <renan.merlo@grainger.com>
2026-04-29 22:26:18 -0300

    Done section 1.

---
+++ fundamentals/01-why-typescript.js
@@ -0,0 +1,6 @@
+"use strict";
+const courseName = "Typescript Bootcamp V2";
+printCourseName(courseName);
+function printCourseName(name) {
+    console.log("The course name is " + name.toUpperCase());
+}
---
+++ fundamentals/01-why-typescript.ts
@@ -0,0 +1,8 @@
+const courseName = "Typescript Bootcamp V2"
+
+printCourseName(courseName)
+
+function printCourseName(name :string) {
+    console.log("The course name is " + name.toUpperCase());
+}
+
